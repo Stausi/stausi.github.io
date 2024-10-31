6f953f853adb793667d57036c0872d9dbdafb76f
Stausi <jonasbs@live.dk>
2024-10-31 13:45:08 +0100

    Search API settings

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,7 +3,7 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search: React.FC = () => {
-  return <DocSearch appId="8XXXVNJ7UW" apiKey="bf7661918f784d59105bc70fcc22bfb0" indexName="overextended" />;
+  return <DocSearch appId="W6V9JVS5SU" apiKey="3eb4544013afecb38b276148364bf788" indexName="stausi" />;
 };
 
 export default Search;
